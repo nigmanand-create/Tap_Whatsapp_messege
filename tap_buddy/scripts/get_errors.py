b5e3294cfb539746e1e4e4faaa3b51eca58896b9
Nigmanand Das <nigmanand@navgurukul.org>
2026-06-24 03:14:07 +0530

    chore: remove all old one-off scripts in tap_buddy/scripts

--- tap_buddy/scripts/get_errors.py
+++
@@ -1,8 +0,0 @@
-import frappe
-def run():
-    errors = frappe.get_all("Error Log", fields=["error", "method", "creation"], order_by="creation desc", limit=100)
-    for e in errors:
-        if "phone_number" not in e.method:
-            print(f"[{e.creation}] --- Method: {e.method} ---")
-            lines = e.error.strip().split('\n')
-            print(lines[-1] if lines else "")
--- tap_buddy/scripts/update_glific_flow_doctype.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def run():
-    doc = frappe.get_doc("DocType", "Glific Flow")
-    doc.custom = 0
-    doc.save(ignore_permissions=True)
-    frappe.db.commit()
-    print("Glific Flow DocType is now standard")
-
